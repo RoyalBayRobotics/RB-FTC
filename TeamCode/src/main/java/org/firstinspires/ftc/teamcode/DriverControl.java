61abe0570f772ca831d53f4ad5bfc16ee7bbe84e
Rio6 <rio6.liu@gmail.com>
2017-01-24 13:48:54 -0800

    New class DriverControl

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControl.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * Created by Rio
+ */
+
+public class DriverControl extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
