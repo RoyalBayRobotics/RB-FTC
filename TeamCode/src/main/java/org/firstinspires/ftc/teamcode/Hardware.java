4964ba5ad426a8fa4334953b0919e0130edf0204
Rio6 <rio6.liu@gmail.com>
2018-01-22 16:05:22 -0800

    Not using java 8

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -15,7 +15,7 @@ import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
 class Hardware {
 
     ModernRoboticsI2cRangeSensor rangeSensor;
-    Motor riser;
+    DcMotor riser;
     Servo[] claws = new Servo[2];
     Map<String, DcMotor> wheels = new HashMap<>();
 
@@ -29,18 +29,18 @@ class Hardware {
         claws[0] = hardwares.get(Servo.class, "claw_l");
         claws[1] = hardwares.get(Servo.class, "claw_r");
 
-        wheels.set("fl", hardwares.get(DcMotor.class, "wheel_fl"));
-        wheels.set("fr", hardwares.get(DcMotor.class, "wheel_fr"));
-        wheels.set("bl", hardwares.get(DcMotor.class, "wheel_bl"));
-        wheels.set("br", hardwares.get(DcMotor.class, "wheel_br"));
+        wheels.put("fl", hardwares.get(DcMotor.class, "wheel_fl"));
+        wheels.put("fr", hardwares.get(DcMotor.class, "wheel_fr"));
+        wheels.put("bl", hardwares.get(DcMotor.class, "wheel_bl"));
+        wheels.put("br", hardwares.get(DcMotor.class, "wheel_br"));
 
         // Init them
         riser.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         riser.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        wheels.forEach((k, motor) -> {
+        for(DcMotor motor : wheels.values()) {
             motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        });
+        }
     }
 }
