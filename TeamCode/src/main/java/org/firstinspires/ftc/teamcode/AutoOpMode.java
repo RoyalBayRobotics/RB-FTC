f4b006dbdf1cda0562cf269cfd2753914f46c25d
Rio6 <rio6.liu@gmail.com>
2018-02-14 17:24:33 -0800

    Improve autonomous movement

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOpMode.java
@@ -19,9 +19,11 @@ class AutoOpMode extends LinearOpMode {
     protected double startRot = 180;
     protected String side = "red";
 
+    private Hardware hw;
+
     @Override
     public void runOpMode() {
-        Hardware hw = new Hardware(hardwareMap, this);
+        hw = new Hardware(hardwareMap, this);
         VuMarkIdentifier vMarkId = new VuMarkIdentifier(hardwareMap, this);
 
         hw.moveClaw(Hardware.CLAW_OPEN);
@@ -33,32 +35,56 @@ class AutoOpMode extends LinearOpMode {
         hw.setArmPosition(.3f, 1);
 
         RelicRecoveryVuMark vm = vMarkId.findVuMark();
-        double tgtDist = getDistanceToColumn(vm);
-        int wallSide = side == "blue" ? 1 : -1;
 
-        hw.turnAngle(startRot, .5f);
+        hw.turnAngle(startRot, 1);
+
+        sleep(1000);
+
+        double tgtDist = getDistanceToColumn(vm);
+        toDistance(tgtDist);
+
+        sleep(1000);
+
+        hw.driveDistance(10, 1);
+
+        sleep(1000);
+
+        hw.moveClaw(Hardware.CLAW_RELEASE);
+
+        sleep(1000);
+
+        hw.driveDistance(-10, 1);
+
+        sleep(1000);
+
+        tgtDist = getDistanceToColumn(RelicRecoveryVuMark.CENTER);
+        toDistance(tgtDist);
+
+        sleep(1000);
+
+        hw.driveDistance(10, 1);
+    }
+
+    private void toDistance(double tgtDist) {
+        int wallSide = side == "blue" ? 1 : -1;
 
         while(opModeIsActive()) {
             double distance = hw.rangeSensor.getDistance(DistanceUnit.CM);
-            telemetry.addData("Target", tgtDist + ": " + distance + " a: " + opModeIsActive());
+            telemetry.addData("Target", tgtDist);
             if(distance < tgtDist) {
                 hw.drive(Math.copySign(.3, wallSide), 0, 0);
                 telemetry.addData("Target", "Farther: " + distance);
             } else if(distance > tgtDist) {
-                hw.drive(.3 * Math.copySign(.3, -wallSide), 0, 0);
+                hw.drive(Math.copySign(.3, -wallSide), 0, 0);
                 telemetry.addData("Target", "Closer: " + distance);
             } else {
                 telemetry.addData("Target", "Here: " + distance);
+                hw.drive(0, 0, 0);
                 break;
             }
             telemetry.update();
         }
-
         telemetry.update();
-
-        hw.driveDistance(10, 1);
-
-        hw.moveClaw(Hardware.CLAW_RELEASE);
     }
 
     private double getDistanceToColumn(RelicRecoveryVuMark vm) {
