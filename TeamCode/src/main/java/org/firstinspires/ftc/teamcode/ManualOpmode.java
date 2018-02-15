deee4e5c9d0bceac72148000cb9448ccb203b161
Rio6 <rio6.liu@gmail.com>
2018-02-14 17:26:01 -0800

    Improve claw control in manual opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualOpmode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualOpmode.java
@@ -1,5 +1,5 @@
 /*
- * ManualOpmode.jaba
+ * ManualOpmode.java
  * Author: Rio
  * Date: 2018/02/04
  *
@@ -14,6 +14,8 @@ import com.qualcomm.robotcore.eventloop.opmode.*;
 public class ManualOpmode extends OpMode {
 
     private Hardware hw;
+    private boolean bumperPushed = false;
+    private boolean isClawOpen = true;
 
     public void init() {
         hw = new Hardware(hardwareMap, null);
@@ -22,6 +24,16 @@ public class ManualOpmode extends OpMode {
     public void loop() {
         hw.drive(gamepad1.right_stick_x, gamepad1.left_stick_y, gamepad1.left_stick_x);
         hw.moveArm((gamepad1.right_bumper ? 1f : 0f) - gamepad1.right_trigger);
-        hw.moveClaw(gamepad1.left_trigger);
+
+        if(gamepad1.left_bumper) {
+            if(!bumperPushed) {
+                isClawOpen = !isClawOpen;
+            }
+            bumperPushed = true;
+        } else {
+            bumperPushed = false;
+        }
+
+        hw.moveClaw(isClawOpen ? 1 - gamepad1.left_trigger : gamepad1.left_trigger);
     }
 }
